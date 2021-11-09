671ae5bc915acc9f546cd2784e1001ddf9085956
Jahnke <naterased@gmail.com>
2021-11-09 13:44:09 -0600

    created json for fighters, tested component pulling in data from json. next create json data for event.

--- ufc-event-react/src/Components/Fight.js
+++ ufc-event-react/src/Components/Fight.js
@@ -1,23 +1,23 @@
 import React from "react";
 
-const Fight = (props) => 
-        <div className="fight-container">
-        <div className="content">
-           <div className="parent">
-             <div className="content-overlay"></div>
-            <img src={props.fighter1_image}
-              alt="" className="over-img-left small-img" />
-            <img src={props.fighter2_image}
-              alt="" className="over-img-right small-img" />
-            <div className="content-details fadeIn-bottom">
-              <h3 className="content-title">{props.fighter1_name}</h3>
-              <div className="enhanced-hr">
-                <span>VS</span>
-              </div>
-              <h3 className="content-title">{props.fighter2_name}</h3>
-            </div>
+const Fight = (props) =>
+  <div className="fight-container">
+    <div className="content">
+      <div className="parent">
+        <div className="content-overlay"></div>
+        <img src={props.fighter1_image}
+          alt="" className="over-img-left small-img" />
+        <img src={props.fighter2_image}
+          alt="" className="over-img-right small-img" />
+        <div className="content-details fadeIn-bottom">
+          <h3 className="content-title">{props.fighter1_fname} {props.fighter1_lname}</h3>
+          <div className="enhanced-hr">
+            <span>VS</span>
           </div>
+          <h3 className="content-title">{props.fighter2_fname} {props.fighter2_lname}</h3>
         </div>
-      </div>;
+      </div>
+    </div>
+  </div>;
 
 export default Fight;
