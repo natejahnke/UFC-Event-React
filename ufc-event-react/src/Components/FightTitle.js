3dc24084c3b80aab980e6c8a4aeeb2deca22f50b
Jahnke <naterased@gmail.com>
2021-11-30 15:55:35 -0600

    updating Event title. work in progress.

--- ufc-event-react/src/Components/FightTitle.js
+++ ufc-event-react/src/Components/FightTitle.js
@@ -2,16 +2,16 @@ import React from "react";
 
 const FightTitle = (props) => {
 
-return (
-    <div className="fight-title">
-        <h1 className="fight-title-number">UFC 268</h1>
-        <div className="fight-title-names">
-            <p className="fight-title-name1">USMAN</p>
-            <p>vs</p>
-            <p className="fight-title-name2">COVINGTON</p>
+    return (
+        <div className="fight-title">
+            <h1 className="fight-title-number">UFC 268</h1>
+            <div className="fight-title-names">
+                <p className="fight-title-name1">USMAN</p>
+                <p className="fight-title-versus">vs</p>
+                <p className="fight-title-name2">COVINGTON</p>
+            </div>
         </div>
-    </div>
-);
+    );
 };
 
 export default FightTitle;
